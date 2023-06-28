50be1243f02592acd29c3728d8ef873fad72bdc9
asad2200 <asadjakhavala92@gmail.com>
2023-06-28 10:48:44 +0530

    🙍‍♂️ Added user profile pic and added logout on click of it

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,6 +1,11 @@
+'use client'
+
+import { useSession, signOut } from "next-auth/react"
 import NewChat from "./NewChat"
 
 function SideBar() {
+    const { data: session } = useSession()
+
     return (
         <div className="p-2 flex flex-col h-screen bg-gray-500/20">
             <div className="flex-1">
@@ -14,6 +19,15 @@ function SideBar() {
                     {/* Chat Rows */}
                 </div>
             </div>
+
+            {session && (
+                <img 
+                    onClick={() => {signOut()}}
+                    src={session.user?.image!}
+                    className="h-16 w-16 p-2 border border-gray-100/50 rounded-full mx-auto mb-2 
+                    cursor-pointer hover:opacity-50"
+                />
+            )}
         </div>
     )
 }
