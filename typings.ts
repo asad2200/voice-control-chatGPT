ded92b99921045c649d1b1d14d8aefbe8f1d883f
asad2200 <asadjakhavala92@gmail.com>
2023-06-30 01:24:25 +0530

    🤪 Fixed last error

--- typings.ts
+++ typings.ts
@@ -1,8 +1,9 @@
 import admin from 'firebase-admin'
+import { FieldValue } from 'firebase/firestore'
 
 export interface Message {
     text: string
-    createdAt: admin.firestore.Timestamp
+    createdAt: admin.firestore.Timestamp | FieldValue
     user: {
         _id: string,
         name: string,
